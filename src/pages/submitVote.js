7bc9b9ab87c849d19cdee50ceb170a56d14ab1d6
Stockbridge <chris.d.stockbridge@gmail.com>
2019-02-05 21:08:01 -0800

    converting to text fields

--- src/pages/submitVote.js
+++ src/pages/submitVote.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
-
+import { Button, TextField } from '@material-ui/core';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
@@ -8,10 +8,26 @@ import SEO from '../components/seo';
 const SubmitVotePage = () => (
   <Layout>
     <SEO title="Submit Vote on Item" />
-    <p>Vote</p>
-    <p>Style</p>
-    <p>Brand</p>
-    <p>Comments</p>
+    <form>
+      <div style={{marginBottom: "10px"}}>
+        <TextField variant="outlined" label="Vote" placeholder="(1-10)" type="range" max="10" min="0" required={ true } fullWidth={ true } name="ItemName"></TextField>
+      </div>
+      <div style={{marginBottom: "10px"}}>
+        <TextField variant="outlined" label="Name" placeholder="(i.e. Cherry Blossom, Indian Well Red, etc.)" fullWidth={ true } name="ItemName"></TextField>
+      </div>
+      <div style={{marginBottom: "10px"}}>
+        <TextField variant="outlined" label="Style" placeholder="(i.e. Merlot, Pinot Noir, etc.)" fullWidth={ true } name="ItemStyle"></TextField>
+      </div>
+      <div style={{marginBottom: "10px"}}>
+        <TextField variant="outlined" label="Brand" placeholder="(i.e. Trader Gioto's, Chateau Ste. Michelle, etc)" fullWidth={ true } name="Item Brand"></TextField>
+      </div>
+      <div style={{marginBottom: "10px"}}>
+        <TextField variant="outlined" label="Comments" placeholder="(i.e. Cost, Tasting Notes, etc.)" fullWidth={ true }></TextField>
+      </div>
+      <div style={{marginBottom: "10px"}}>
+        <Button type="submit" fullWidth={ true }>Submit Vote on Item</Button>
+      </div>
+    </form>
     <Link to="/">Back</Link>
   </Layout>
 );
